a4f1860b8ec181a53bab51cc191ff5dd59d05b08
Mehdi <semlali.mehdi.1980@gmail.com>
2021-04-11 17:52:10 +0200

    changing from == to strict equality operator ===

--- algorithms/Arrays/simple/fizzBuzz.js
+++ algorithms/Arrays/simple/fizzBuzz.js
@@ -7,10 +7,10 @@ const fizzBuzz = (num) => {
     for (let i = 0; i <= num; i++) {
         let result = i;
 
-        // if(i > 0 && i % 15 == 0) {result = "FizzBuzz";}
-        if (i > 0 && i % 3 == 0 && i % 5 == 0) { result = "FizzBuzz"; }
-        else if (i > 0 && i % 3 == 0) { result = "Fizz"; }
-        else if (i > 0 && i % 5 == 0) { result = "Buzz"; }
+        // if(i > 0 && i % 15 === 0) {result = "FizzBuzz";}
+        if (i > 0 && i % 3 === 0 && i % 5 === 0) { result = "FizzBuzz"; }
+        else if (i > 0 && i % 3 === 0) { result = "Fizz"; }
+        else if (i > 0 && i % 5 === 0) { result = "Buzz"; }
 
         console.log(result);
 
