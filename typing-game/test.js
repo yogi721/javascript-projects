538224770c10dc57f16bf75d520e037c95b534fa
Mehdi <semlali.mehdi.1980@gmail.com>
2021-04-19 03:55:32 +0200

    testing different implementation for typing sctipt

--- typing-game/test.js
+++ typing-game/test.js
@@ -2,7 +2,28 @@ const statsDiv = document.getElementById("stats");
 const typingDiv = document.getElementById("typing");
 const startGameBtn = document.getElementById("start-game");
 
-const paragraphs = `look straight ahead. make sure your eyes look directly into your monitor the same way you would look at someone across the table from you.`;
+const paragraph = `look straight ahead. make sure your eyes look directly into your monitor the same way you would look at someone across the table from you.`;
 
 
 const startGame = () => { }
+const getText = (paragraph) => {
+    let pg = paragraph.split('')
+    return pg
+}
+const getWords = (paragraph) => {
+    let pg = paragraph.split(' ')
+    return pg
+}
+
+const getWordsWithoutSign = (paragraph) => {
+    let pg = getWords(paragraph)
+    pg.forEach((word, i) => {
+        pg[i] = word.replace(/[^a-zA-Z ]/g, "")
+    });
+
+    return pg
+}
+
+const isTextEnd = () => { }
+
+console.log(getWordsWithoutSign(paragraph))
