7aa5cb6bfa0f277624844a2aa929d7d05a682ed7
Mehdi <semlali.mehdi.1980@gmail.com>
2021-05-03 00:11:27 +0200

    updating switch file

--- algorithms/controlFlow/switch.js
+++ algorithms/controlFlow/switch.js
@@ -10,13 +10,42 @@
 //         break;
 // }
 
-switch (fruittype) {
-    case 'Oranges':
-        console.log('Oranges are $0.59 a pound. ');
+
+// switch (fruittype) {
+//     case 'Oranges':
+//         console.log('Oranges are $0.59 a pound. ');
+//         break;
+//     case 'Apples':
+//         console.log('Apples are $0.32 a pound.');
+//         break;
+//     default:
+//         console.log(`Sorry, we are out of ${fruittype}.`);
+// }
+
+let day;
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
         break;
-    case 'Apples':
-        console.log('Apples are $0.32 a pound.');
+    case 1:
+        day = "Monday";
         break;
-    default:
-        console.log(`Sorry, we are out of ${fruittype}.`);
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
 }
+
+console.log('Today is: ' + day);
