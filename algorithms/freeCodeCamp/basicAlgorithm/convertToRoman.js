95466857edb3b0d6be27f4515669713bc1ab3598
Mehdi <semlali.mehdi.1980@gmail.com>
2021-05-11 05:14:45 +0200

    convertToRoman

---
+++ algorithms/freeCodeCamp/basicAlgorithm/convertToRoman.js
@@ -0,0 +1,18 @@
+function convertToRoman(num) {
+    let result = ''
+
+    let romNum = {
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X',
+    }
+
+}
+}
